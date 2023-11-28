4d143850aec9acd081ae45d70584a42f43c51bf2
beetrbgus <devbeetrb0214@gmail.com>
2023-11-28 16:26:57 +0900

    node_module tracking 제외

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-const http = require('http');
-
-const hostname = 'localhost';
-const port = 3000;
-
-var server = http.createServer(function(request,response){ 
-
-    response.writeHead(200,{'Content-Type':'text/plain'});
-    response.end('Hello node.js!!');
-
+const express = require('express');
+const app = express()
+app.get('/', function(req, res) {
+    res.send('Hello world');
 });
 
-// 3. listen 함수로 8080 포트를 가진 서버를 실행한다. 서버가 실행된 것을 콘솔창에서 확인하기 위해 'Server is running...' 로그를 출력한다
-server.listen(port, function(){ 
-    console.log('Server is running...');
-});
+app.listen(3000);
