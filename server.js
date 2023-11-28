e164bbeafa14cd9efc2b72c1c1e720eac4525c4c
beetrbgus <devbeetrb0214@gmail.com>
2023-11-28 17:15:49 +0900

    feat- post 시 body 받는 방법

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', function(req, res) {
     res.send('Hello world');
 });
 
+// GET 요청을 받는 방법
 app.get('/:animal', function(req, res) {
     // routing 경로로 받은 값
     const animal = req.params.animal;
@@ -19,6 +20,11 @@ app.get('/:animal', function(req, res) {
     res.json(sound);
 });
 
+// POST 요청을 받는 방법
+app.post('/:animal', (req, res) => {
+    const body = req.body;
+    console.log(body);
+})
 app.listen(port, ()=> {
     console.log(`Example app listening on port ${port}`);
 }); 
