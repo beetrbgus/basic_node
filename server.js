d43387782561ca920d9605c0108e57ba9fb4928e
beetrbgus <devbeetrb0214@gmail.com>
2023-11-28 16:58:52 +0900

    라우트 예제 만들기

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ app.get('/', function(req, res) {
     res.send('Hello world');
 });
 
+app.get('/dog', function(req, res) {
+    let sound = {
+        'sound' : '멍멍'
+    }
+    res.send(sound);
+});
+
+app.get('/cat', function(req, res) {
+    let sound = {
+        'sound' : '야옹'
+    }
+    res.send(sound);
+});
+
 app.listen(port, ()=> {
     console.log(`Example app listening on port ${port}`);
 }); 
